678990f448eef18634a79b385d79027792cd6780
TheLitFire <zhao1697801560@gmail.com>
2019-05-11 16:56:23 +0800

    Update ANOVA_offload_v1.c

--- code/thelitfire/ANOVA_offload_v1.c
+++ code/thelitfire/ANOVA_offload_v1.c
@@ -25,7 +25,8 @@ static __always_inline __u32 double_to_u32(__u64 x){
 
 static __always_inline __u64 mul(__u32 a){
     __u16 a2=a>>16,a1=(a<<16)>>16;
-    return (__u64)((__u32)a1*a1)+(((__u64)((__u32)a2*a1)+(__u64)((__u32)a1*a2)<<16))+(__u64)((__u32)a2*a2<<32);
+    //return (__u64)((__u32)a1*a1)+(((__u64)((__u32)a2*a1)+(__u64)((__u32)a1*a2))<<16)+(__u64)((__u32)a2*a2<<32);
+    return (__u32)a1*a1;
 }
 
 SEC("xdp")

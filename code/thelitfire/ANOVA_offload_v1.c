4401f99cd9b0ab245677cf0eca4d0892b48b5d51
TheLitFire <zhao1697801560@gmail.com>
2019-05-11 15:30:01 +0800

    Update ANOVA_offload_v1.c

--- code/thelitfire/ANOVA_offload_v1.c
+++ code/thelitfire/ANOVA_offload_v1.c
@@ -9,87 +9,17 @@
 #include <stdbool.h>
 #include "bpf_helpers.h"
 #include "bpf_endian.h"
-//constant//
+
 #define MAGIC 0x216C7174786A7A21ULL
-//type//
+
 struct packet_struct {
     __u64 magic;
     __u64 tag;
     __u64 data[32];
 };
-//functions//
-/*
-double_to_u32: Programmed by censyu
-double:
-63    | 62            52 | 51     0 |
-sign  | 11 bits exponent | mantissa |
-__u32 x:
-31               25           10         0
-0 ......        2^15         2^0       2^-10
-*/
-/*
-static __always_inline __u32 double_to_u32(__u64 data_double){
-    __u32 exponent;
-    __u64 x;
-    if (data_double==(__u64)0) x=(__u64)0;
-    exponent=(__u32)(data_double >> 52)-(__u32)1023;
-    x=((data_double&(__u64)0x000FFFFFFFFFFFFF)|(__u64)0x0010000000000000)>>(((__u32)1065>(__u32)(data_double >> 52))?(__u32)1065-(__u32)(data_double >> 52):(__u32)(data_double >> 52)-(__u32)1065);
-    return (__u32)x;
-}
-*/
+
 #define double_to_u32(x) ((x==(__u64)0)?(__u32)0:(__u32)(((x&(__u64)0x000FFFFFFFFFFFFF)|(__u64)0x0010000000000000)>>(((__u32)1065>(__u32)(x >> 52))?(__u32)1065-(__u32)(x >> 52):(__u32)(x >> 52)-(__u32)1065)))
-/*
-static __always_inline bool parse_fjw(void *data, __u64 off, void *data_end){
-    struct packet_struct *raw;
-    raw = data + off;
-    if (raw + 1 > data_end) return false;
-    if (raw->magic != MAGIC) {  // !fjwtql!
-        return false;
-    } else {
-        if (raw->tag != 0) return false;
-//for first 16 data (type __u32), calculate mean and var
-        __u32 dt,mean;
-        __u64 var;
-        #pragma clang loop unroll(full)
-        for (int i = 0; i < 16; i++) {
-            dt=double_to_u32(raw->data[i]);
-            mean+=dt;
-            var+=(__u64)dt*(__u64)dt;
-        }
-        mean>>=4;var>>=4;
-        var-=(__u64)mean*mean;
-        const __u32 e=0x66;
-        const __u64 e2=0x28A4;
-        __u32 tot;
-        #pragma clang loop unroll(full)
-        for (int i=16;i<32;++i){
-            dt=double_to_u32(raw->data[i]);
-            tot+=( dt>=mean ? (dt-mean>=e ? (__u32)1 : (__u32)0) : (mean-dt>=e ? (__u32)1 : (__u32)0) );
-        }
-        if ((__u64)tot*e2 > (var<<4)){
-            raw->tag = (__u64)1;// hua fen！
-        }else{
-            raw->tag = (__u64)2;// no hua fen
-        }
-    }
-    return true;
-}*/
-/*
-static __always_inline int modify_tag(void *data, __u64 off, void *data_end) {
-    struct packet_struct *raw;
 
-    raw = data + off;
-    if (raw + 1 > data_end)
-        return false;
-
-    if (raw->magic != MAGIC) {
-        return false;
-    } else { 
-        raw->tag = 0;
-        return XDP_TX;
-    }
-}
-*/
 SEC("xdp")
 int process_packet(struct xdp_md *ctx)
 {
@@ -125,7 +55,7 @@ int process_packet(struct xdp_md *ctx)
         mean>>=4;var>>=4;var-=(__u64)mean*mean;
         const __u32 e=0x66;
         const __u64 e2=0x28A4;
-        __u32 tot;
+        __u32 tot=(__u32)0;
 
         dt=double_to_u32(raw->data[16]);tot+=( dt>=mean ? (dt-mean>=e ? (__u32)1 : (__u32)0) : (mean-dt>=e ? (__u32)1 : (__u32)0) );
         dt=double_to_u32(raw->data[17]);tot+=( dt>=mean ? (dt-mean>=e ? (__u32)1 : (__u32)0) : (mean-dt>=e ? (__u32)1 : (__u32)0) );
